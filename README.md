29f5b8414709d56fb03d83139cb3b85b8bab0923
Jam Risser <jamrizzi@gmail.com>
2016-12-07 16:19:41 -0600

    Greatly enhanced api

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ npm install
         blog: process.env.GPROXY_BLOG || 'myblog.com', // location of the blog using the ghost proxy
         username: process.env.GPROXY_USERNAME || 'username', // ghost login username
         password: process.env.GPROXY_PASSWORD || 'password', // ghost login password
+				clientSecret: process.env.GPROXY_CLIENT_SECRET || 'c38dcd39fb6f', // ghost login client_secret
         blacklist: process.env.GPROXY_BLACKLIST ? process.env.GPROXY_BLACKLIST.replace(' ', '').split(',') : [ // array of blacklisted endpoints
             '/users'
         ],
@@ -38,7 +39,7 @@ npm install
 ## Docker
 View on [Docker](https://hub.docker.com/r/jamrizzi/ghost-proxy).
 ### Usage
-    
+
 ```
 sudo docker run --name ghost-proxy -d -p 3008:3008 -e GPROXY_GHOST=myghost.com -e GPROXY_USERNAME=username -e GPROXY_PASSWORD=password jamrizzi/ghost-proxy:latest
 ```
@@ -50,6 +51,7 @@ sudo docker run --name ghost-proxy -d -p 3008:3008 -e GPROXY_GHOST=myghost.com -
 * GPROXY_PASSWORD - ghost login password
 * GPROXY_BLACKLIST - comma separated list of blacklisted endpoints
 * GPROXY_PORT - port of the ghost proxy server
+* GPROXY_CLIENT_SECRET - ghost login client_secret (set to 'c38dcd39fb6f' by default)
 
 ## Planned Features
 * Advanced Blacklist Rules
--- app.js
+++ app.js
@@ -1,163 +1,187 @@
 var express = require('express');
 var Promise = require('bluebird');
-var request = require('request');
+var fetch = require('node-fetch');
+var _ = require('lodash');
+var winston = require('winston');
+var Err = require('err');
 var app = express();
 var bearerToken = '';
 
 var options = {
-    ghost: process.env.GPROXY_GHOST || '', // location of your ghost installation
-    blog: process.env.GPROXY_BLOG || '', // location of the blog using the ghost proxy
-    username: process.env.GPROXY_USERNAME || '', // ghost login username
-    password: process.env.GPROXY_PASSWORD || '', // ghost login password
-    blacklist: process.env.GPROXY_BLACKLIST ? process.env.GPROXY_BLACKLIST.replace(' ', '').split(',') : [ // array of blacklisted endpoints
-        '/users'
-    ],
-    port: process.env.GPROXY_PORT || 3008 // port for the ghost proxy server
+  ghost: process.env.GPROXY_GHOST || '', // location of your ghost installation
+  blog: process.env.GPROXY_BLOG || '', // location of the blog using the ghost proxy
+  username: process.env.GPROXY_USERNAME || '', // ghost login username
+  password: process.env.GPROXY_PASSWORD || '', // ghost login password
+	clientSecret: process.env.GPROXY_CLIENT_SECRET || 'c38dcd39fb6f', // ghost login client_secret
+  blacklist: process.env.GPROXY_BLACKLIST ? process.env.GPROXY_BLACKLIST.replace(' ', '').split(',') : [ // array of blacklisted endpoints
+    '/users'
+  ],
+  port: process.env.GPROXY_PORT || 3008 // port for the ghost proxy server
 };
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-app.get('/', function(req, res) {
-    res.redirect(options.blog);
-});
-
-app.get('/:one', function(req, res) {
-    var slug = '/' + req.params.one;
-    if (!isBlacklisted(slug)) {
-        ghostRequest(slug)
-        .then(function(body) {
-            res.json(body);
-        }).catch(function(err) {
-            sendError(res, err);
-        });
-    } else {
-        res.status(403).send('Access denied');
-    }
-});
-
-app.get('/:one/:two', function(req, res) {
-    var slug = '/' + req.params.one + '/' + req.params.two;
-    if (!isBlacklisted(slug)) {
-        ghostRequest(slug)
-        .then(function(body) {
-            res.json(body);
-        }).catch(function(err) {
-            sendError(res, err);
-        });
-    } else {
-        res.status(403).send('Access denied');
-    }
-});
-
-app.get('/:one/:two/:three', function(req, res) {
-    var slug = '/' + req.params.one + '/' + req.params.two + '/' + req.params.three;
-    if (!isBlacklisted(slug)) {
-        ghostRequest(slug)
-        .then(function(body) {
-            res.json(body);
-        }).catch(function(err) {
-            sendError(res, err);
-        });
-    } else {
-        res.status(403).send('Access denied');
-    }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.listen(options.port, function() {
-    console.log('serving application');
+	_.each(options, function(option, key) {
+		console.log(key + ': ' + option);
+	});
 });
 
-function sendError(res, err) {
-    if (err.statusCode) {
-        if (err.body) {
-            res.status(err.statusCode).send(err.body);
-        } else {
-            res.status(err.statusCode).send(err);
-        }
-    } else {
-        console.log(err);
-        res.status(400).send(err);
-    }
+app.get('/', function(req, res) {
+  res.redirect(options.blog);
+});
+
+app.get('/:one', function(req, res) {
+	return new Promise(function(resolve, reject) {
+		resolve('/' + req.params.one);
+	}).then(function(slug) {
+		if (slug === '/favicon.ico') {
+			return res.json({message: 'Rest api does not serve favicon'});
+		}
+		if (!isBlacklisted(slug)) {
+			return ghostRequest(slug)
+				.then(function(body) {
+					res.json(body);
+				}).catch(function(err) {
+					throw err;
+				});
+		} else {
+			throw new Err('Access denied', 403);
+		}
+	}).catch(function(err) {
+		handleError(err, res);
+	});
+});
+
+app.get('/:one/:two', function(req, res) {
+	return new Promise(function(resolve, reject) {
+		return '/' + req.params.one + '/' + req.params.two;
+	}).then(function(slug) {
+		if (!isBlacklisted(slug)) {
+			return ghostRequest(slug)
+				.then(function(body) {
+					res.json(body);
+				}).catch(function(err) {
+					throw err;
+				});
+		} else {
+			throw new Err('Access denied', 403);
+		}
+	}).catch(function(err) {
+		handleError(err, res);
+	});
+});
+
+app.get('/:one/:two/:three', function(req, res) {
+	return new Promise(function(resolve, reject) {
+		return '/' + req.params.one + '/' + req.params.two + '/' + req.params.three;
+	}).then(function(slug) {
+		if (!isBlacklisted(slug)) {
+			return ghostRequest(slug)
+				.then(function(body) {
+					res.json(body);
+				}).catch(function(err) {
+					throw err;
+				});
+		} else {
+			throw new Err('Access denied', 403);
+		}
+	}).catch(function(err) {
+		handleError(err, res);
+	});
+});
+
+function handleError(err, res) {
+	if (err.code) {
+    var code = Number(err.code);
+		if (100 <= code && code <= 599) { // valid http response code
+		  if (code >= 500) { // server error
+			  winston.error(err);
+			} else {
+				if (process.env.NODE_ENV !== 'production') {
+          winston.warn(err.message);
+				}
+			}
+		  return res.status(err.code).json({message: err.message});
+		}
+	}
+	// unknown error
+	winston.error(err);
+	return res.status(500).json({message: err.message});
 }
 
 function isBlacklisted(item) {
-    for (var i = 0; i < options.blacklist.length; i++) {
-        var blacklisted = options.blacklist[i];
-        if (item === blacklisted) {
-            return true;
-        }
+  for (var i = 0; i < options.blacklist.length; i++) {
+    var blacklisted = options.blacklist[i];
+    if (item === blacklisted) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function getBearerToken() {
-    return new Promise(function(resolve, reject) {
-        request({
-            url: 'http://blog.groupthreads.com/ghost/api/v0.1/authentication/token',
-            method: 'POST',
-            json: true,
-            body: {
-                grant_type: 'password',
-                username: options.username,
-                password: options.password,
-                client_id: 'ghost-admin',
-                client_secret: 'c38dcd39fb6f'
-            }
-        }, function (err, res, body) {
-            if (!err) {
-                if (res.statusCode == 200) {
-                    resolve(body.access_token);
-                } else {
-                    reject(res);
-                }
-            } else {
-                reject(err);
-            }
-        });
-    });
+	return fetch(options.ghost + '/ghost/api/v0.1/authentication/token', {
+    method: 'POST',
+    body: JSON.stringify({
+      grant_type: 'password',
+      username: options.username,
+      password: options.password,
+      client_id: 'ghost-admin',
+      client_secret: options.clientSecret
+    }),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(function(response) {
+		if (response.status == 200) {
+			return response.json();
+		} else {
+			throw new Err('Could not fetch token', response.status);
+		}
+	}).then(function(body) {
+		return body.access_token;
+	}).catch(function(err) {
+		throw err;
+	});
 }
 
 function ghostRequest(slug) {
-    return new Promise(function(resolve, reject) {
-        request({
-            url: options.ghost + '/ghost/api/v0.1' + slug,
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer ' + bearerToken
-            }
-        }, function (err, res, body) {
-            if (!err) {
-                if (res.statusCode == 200) {
-                    try {
-                        resolve(JSON.parse(body));
-                    } catch(err) {
-                        reject({
-                            statusCode: 404,
-                            body: err
-                        });
-                    }
-                } else if (res.statusCode === 401) {
-                    getBearerToken().then(function(token) {
-                        bearerToken = token;
-                        ghostRequest(slug).then(function(body) {
-                            resolve(body);
-                        }).catch(function(err) {
-                            reject(err);
-                        });
-                    }).catch(function(err) {
-                        reject(false);
-                    });
-                } else {
-                    reject(res);
-                }
-            } else {
-                reject(err);
-            }
-        });
-    });
+	var url = options.ghost + '/ghost/api/v0.1' + slug;
+	winston.info('requesting ' + url);
+	return fetch(url, {
+		headers: {
+			Authorization: 'Bearer ' + this.bearerToken
+		}
+	}).then(function(response) {
+		if (response.status == 200) {
+			return response.text();
+		} else if (response.status == 401) {
+			return getBearerToken().then(function(token) {
+				this.bearerToken = token;
+				return ghostRequest(slug).then(function(body) {
+					return body;
+				}).catch(function(err) {
+					throw err;
+				});
+			}).catch(function(err) {
+				reject(err);
+			});
+		} else {
+			throw new Err('Unknown error', response.status);
+		}
+	}).then(function(body) {
+		try {
+			return JSON.parse(body);
+		} catch(err) {
+			return body;
+		}
+	}).catch(function(err) {
+		throw err;
+	});
 }
